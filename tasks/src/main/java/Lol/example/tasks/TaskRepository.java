a901ec469d0879569850720c28a35353d9f96e37
MObinXIV <mobinxiv@gmail.com>
2025-02-28 19:38:07 +0200

    add the repository for users and tasks relation

--- tasks/src/main/java/Lol/example/tasks/TaskRepository.java
+++ tasks/src/main/java/Lol/example/tasks/TaskRepository.java
@@ -1,2 +1,17 @@
-package Lol.example.tasks;public interface TaskRepository {
+package Lol.example.tasks;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Tasks, UUID> {
+//    List <Tasks> findByUserId(UUID userId);
+
+    @Query("SELECT t FROM Tasks t WHERE t.user.id = :userId")
+    List<Tasks> findByUserId(@Param("userId") UUID userId);
 }
