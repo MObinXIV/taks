9612e9eedff767382edece77505c0762ecb65078
MObinXIV <mobinxiv@gmail.com>
2025-03-06 04:04:49 +0200

    get the user email from the token to start work

--- tasks/src/main/java/Lol/example/tasks/config/JwtAuthenticationFilter.java
+++ tasks/src/main/java/Lol/example/tasks/config/JwtAuthenticationFilter.java
@@ -1,2 +1,46 @@
-package Lol.example.tasks.config;public class JwtAuthenticationFilter {
+package Lol.example.tasks.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private  final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                   @NonNull HttpServletResponse response,
+                                   @NonNull FilterChain filterChain) throws ServletException, IOException {
+// Retrieve the Authorization header from the incoming HTTP request
+        final String authHeader= request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        // If the Authorization header is missing or doesn't start with "Bearer ",
+// allow the request to proceed without authentication instead of throwing an error.
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // Extract the token from the Authorization header
+        jwt = authHeader.substring(7);
+    // Extract user email from the token
+        userEmail = jwtService.extractUserName(jwt);
+
+        filterChain.doFilter(request, response);
+
+    }
 }
